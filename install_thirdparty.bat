fe0f51670b8bbd3609ea599dc833d206bdce92a7
ozxybox <kieferstashu@gmail.com>
2022-01-23 18:51:16 -0600

    Initial Commit

---
+++ install_thirdparty.bat
@@ -0,0 +1,96 @@
+@ECHO OFF
+
+set A_CDSTART=%cd%
+
+echo ==== Checking for required tools ==== 
+echo ======= Installing libraries =======
+echo =========== Building GLFW ===========
+echo =========== Copying GLFW ===========
+
+@REM Git Check
+git --version
+set A_ERRORLEVEL=%ERRORLEVEL%
+if %A_ERRORLEVEL% neq 0 goto missingGit
+
+@REM CMake Check
+cmake --version
+set A_ERRORLEVEL=%ERRORLEVEL%
+if %A_ERRORLEVEL% neq 0 goto missingCMake
+
+echo ======= Installing libraries =======
+rmdir /S /Q thirdparty\win
+mkdir thirdparty\win
+cd thirdparty\win
+
+mkdir temp
+
+echo =========== Building GLFW ===========
+mkdir temp\glfw
+git clone -b 3.3-stable https://github.com/glfw/glfw.git temp\glfw
+
+set A_ERRORLEVEL=%ERRORLEVEL%
+if %A_ERRORLEVEL% neq 0 goto cloneFail
+
+cmake temp\glfw -B temp\glfwbuild -D GLFW_BUILD_EXAMPLES=OFF -D GLFW_BUILD_TESTS=OFF -D GLFW_BUILD_DOCS=OFF
+
+set A_ERRORLEVEL=%ERRORLEVEL%
+if %A_ERRORLEVEL% neq 0 goto cmakeFail
+
+cmake --build temp\glfwbuild --config Release
+
+set A_ERRORLEVEL=%ERRORLEVEL%
+if %A_ERRORLEVEL% neq 0 goto buildFail
+
+echo Copying GLFW
+mkdir glfw
+mkdir glfw\lib
+copy temp\glfwbuild\src\Release\glfw3.lib glfw\lib\glfw3.lib
+robocopy temp\glfw\include glfw\include /E
+
+echo Cleaning up
+rmdir /S /Q temp
+
+echo ==================================================================
+echo Done!
+echo ==================================================================
+cd %A_CDSTART%
+exit /b 0
+
+:missingGit
+echo =============================
+echo Failed to install thirdparty!
+echo Please download Git!
+echo =============================
+goto failEnd
+
+:missingCMake
+echo =============================
+echo Failed to install thirdparty!
+echo Please download CMake!
+echo =============================
+goto failEnd
+
+:cloneFail
+echo =============================
+echo Failed to install thirdparty!
+echo Could not clone repository!
+echo =============================
+goto failEnd
+
+:cmakeFail
+echo =============================
+echo Failed to install thirdparty!
+echo Could not create project!
+echo =============================
+goto failEnd
+
+:buildFail
+echo =============================
+echo Failed to install thirdparty!
+echo Could not build project!
+echo =============================
+goto failEnd
+
+:failEnd
+cd %A_CDSTART%
+exit /b 1
